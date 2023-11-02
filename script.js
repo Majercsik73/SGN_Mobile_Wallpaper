926827598477ee12a3a501d5fe3aced5bb531966
Majercsik73 <riasztotnekem@gmail.com>
2023-11-02 20:12:46 +0100

    inicializálás

---
+++ script.js
@@ -0,0 +1,26 @@
+
+/*
+//Carousel
+var slideIndex = 0;
+function carousel() {
+
+    let x = document.getElementsByClassName("mySlides");
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    
+    slideIndex++;
+    if (slideIndex > x.length) {
+        slideIndex = 1
+    } 
+    x[slideIndex-1].style.display = "block";
+
+    setTimeout(carousel, 4000); 
+}
+
+function Main(){
+    
+    carousel();
+}
+Main();
+*/
